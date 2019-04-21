3cce344918138de4adc14d9241a87805f2bf37a6
mohamed Haridy <s66220@beuth-hochschule.de>
2019-04-21 10:20:32 +0200

    React-1 - small example about how React-router works

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,69 +1,25 @@
 import React, { Component } from 'react';
 import { x as momo } from "./module";
-import Item from "./components/items/Items"
-//import { default as me } from "./module"
+import Item from "./components/items/Items";
+import Nav from "./components/nav/Nav";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import { BrowserRouter, Route } from "react-router-dom";
 
 
 class App extends Component {
-
-  //Create constructor, which will be triggered when the app runned
-  constructor() {
-    // super function should be declared
-    super();
-    { console.log("from con") }
-
-
-  }
-  //componentDidUpdate will be triggered at any change
-  componentDidUpdate(prevProps, prevState) {
-    { console.log("update--->" , prevState) }
-  }
-
-  //componentDidMount will be triggered after rendering the Component
-  componentDidMount() {
-    { console.log("from componentDidMount") }
-  }
-
-  //state to declare and arry or objects calles items 
-  state = {
-    items: [
-      { name: "mohamed", id: "1", age: 25 },
-      { name: "nasser", id: "2", age: 35 },
-      { name: "khaled", id: "3", age: 45 },
-      { name: "khaled", id: "4", age: 45 },
-      { name: "khaled", id: "5", age: 45 }
-    ]
-
-  }
-
-  //handel add Button by pushing new object to the array
-  handleAddButton = () => {
-    let items = this.state.items;
-    items.push({ name: "azza", id: Math.random() * 10, age: "50" });
-    this.setState({
-      items:items
-    });
-  }
-  //handle remove button by remove all items except no. 2
-  handleRemoveButton = () => {
-    let items = this.state.items;
-    items.splice(2);
-    this.setState(items);
-  }
-
-
-
   // Render the Components
   render() {
-    { console.log("render") }
     return (
-      <div className="App">
-        <Item items={this.state.items} />
-        <button onClick={this.handleAddButton}>add</button>
-        <button onClick={this.handleRemoveButton}>Remove</button>
-
-      </div>
-    );
+      //A <Router> that uses the HTML5 history API (pushState, replaceState and the popstate event) to keep your UI in sync with the URL.
+      <BrowserRouter>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
---
+++ react-app/src/components/nav/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//NavBar
+class Nav extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Nav;
