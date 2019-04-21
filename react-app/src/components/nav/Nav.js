450b3fcb38ed4066c40726e2758f48ad8277a8ce
mohamed Haridy <s66220@beuth-hochschule.de>
2019-04-21 10:38:39 +0200

    React-1 - Upgrading react-router by using Navlink

--- react-app/src/components/nav/Nav.js
+++ react-app/src/components/nav/Nav.js
@@ -1,13 +1,16 @@
 import React from "react";
+import {Link,NavLink} from "react-router-dom";
 
 //NavBar
 class Nav extends React.Component {
     render() {
         return (
+            //<Link to> --> like <a href> but preventDefaults() "prevents the page from reloading"
+            //<NavLink to> --> like <Link to> but adds a class called "active" to the selected page, we can rename the active class by using activeClassName 
             <div>
                 <ul>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
+                <NavLink activeClassName="selectedHome" exact to="/">Home</NavLink>
+                <NavLink activeClassName="selectedAbout" to="/about">About</NavLink>
                 </ul>
             </div>
         )
